47cdbd9648543495a5e7b96bf2628bd5c2859cbe
carlosandreslugo58 <93355633+carlosandreslugo58@users.noreply.github.com>
2021-11-01 08:45:28 -0500

    Add files via upload

---
+++ registry-service/src/test/java/cal/almacenV1/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cal.almacenV1.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
